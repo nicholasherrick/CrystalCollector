6bac33fbef00d51158c684f1fea23c368b43241f
Nick Herrick <nth.herrick@gmail.com>
2019-10-20 21:05:30 -0400

    added math and some functions to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 
     // Declare Variables
 
-    var randomNumber = "";
+    var random = "";
     var wins = 0;
     var losses = 0;
-    var crystal1Value = "";
-    var crystal2Value = "";
-    var crystal3Value = "";
-    var crystal4Value = "";
+    var crystal1 = "";
+    var crystal2 = "";
+    var crystal3 = "";
+    var crystal4 = "";
     var totalScore = "";
 
     // function to reset game
@@ -28,8 +28,28 @@ $(document).ready(function() {
 
     // Number randomizer for the random number and each crystal
 
-    // Functions for clicking on each crystal
+    var randomNumber = Math.floor((Math.random() * 120) + 19);
+    var crystal1Value = Math.floor((Math.random() * 12) + 1);
+    var crystal2Value = Math.floor((Math.random() * 12) + 1);
+    var crystal3Value = Math.floor((Math.random() * 12) + 1);
+    var crystal4Value = Math.floor((Math.random() * 12) + 1);
 
+    // Functions for clicking on each crystal
+    $("#crystal-1").on("click", function() {
+        crystal1Value + totalScore
+    });
+
+    $("#crystal-2").on("click", function() {
+        if
+    });
+
+    $("#crystal-3").on("click", function() {
+        if
+    });
+
+    $("#crystal-4").on("click", function() {
+        if
+    });
     // Determine Win/Loss logic
 
     // Html Editors
