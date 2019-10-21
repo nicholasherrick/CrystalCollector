fa0ab65c804d92789ba7dcae8d8c54ee6ba48bcf
Nick Herrick <nth.herrick@gmail.com>
2019-10-20 20:14:59 -0400

    added document.ready function and psuedo code for javascript.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+// Declare Variables
+
+// Number randomizer for the random number and each crystal
+
+// Functions for clicking on each crystal
+
+// Determine Win/Loss logic
+
+// Html Editors
+
+
+
+
+
+
+});
