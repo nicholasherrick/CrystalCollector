cb2cb8c8471817075e76233202eb36a95b9201ec
Nick Herrick <nth.herrick@gmail.com>
2019-10-24 18:13:05 -0400

    added a bunch of javascript that doesn't work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,49 +11,64 @@ $(document).ready(function() {
     var crystal2 = "";
     var crystal3 = "";
     var crystal4 = "";
-    var totalScore = "";
+    var totalScore = 0;
 
     // function to reset game
 
     function resetGame() {
-        randomNumber - "";
+        randomNumber = Math.floor((Math.random() * 120) + 19);
         wins = 0;
         losses = 0;
-        crystal1Value = "";
-        crystal2Value = "";
-        crystal3Value = "";
-        crystal4Value = "";
+        crystal1Value = Math.floor((Math.random() * 12) + 1);
+        crystal2Value = Math.floor((Math.random() * 12) + 1);
+        crystal3Value = Math.floor((Math.random() * 12) + 1);
+        crystal4Value = Math.floor((Math.random() * 12) + 1);
         totalScore = "";
     }
 
     // Number randomizer for the random number and each crystal
 
     var randomNumber = Math.floor((Math.random() * 120) + 19);
+    console.log(randomNumber);
     var crystal1Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal1Value);
     var crystal2Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal2Value);
     var crystal3Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal3Value);
     var crystal4Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal4Value);
+
+    $("#match").html(randomNumber);
 
     // Functions for clicking on each crystal
     $("#crystal-1").on("click", function() {
-        crystal1Value + totalScore
+        crystal1Value + totalScore;
+        $("#score-number").text(totalScore);
     });
 
     $("#crystal-2").on("click", function() {
-        if
+        crystal2Value + totalScore;
     });
 
     $("#crystal-3").on("click", function() {
-        if
+        crystal3Value + totalScore;
     });
 
     $("#crystal-4").on("click", function() {
-        if
+        crystal4Value + totalScore;
     });
     // Determine Win/Loss logic
-
+    if (totalScore > randomNumber) {
+        losses++;
+        alert("You Lost!");
+    }
+    else if (totalScore === randomNumber) {
+        wins++;
+        alert("You Won!");
+    }
     // Html Editors
-
+    
 
 
 
