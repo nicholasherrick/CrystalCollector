3ccd82cb07ea01020fe6d4c5f67ec4fd5fc34b7e
Nick Herrick <nth.herrick@gmail.com>
2019-10-26 10:29:18 -0400

    Ajusted variables in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,20 @@ $(document).ready(function() {
 
     // Declare Variables
 
-    var random = "";
+    var randomNumber = Math.floor((Math.random() * 120) + 19);
+    console.log(randomNumber);
+    var crystal1Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal1Value);
+    var crystal2Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal2Value);
+    var crystal3Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal3Value);
+    var crystal4Value = Math.floor((Math.random() * 12) + 1);
+    console.log(crystal4Value);
+
+    // Win/Loss and Total Score
     var wins = 0;
     var losses = 0;
-    var crystal1 = "";
-    var crystal2 = "";
-    var crystal3 = "";
-    var crystal4 = "";
     var totalScore = 0;
 
     // function to reset game
@@ -28,16 +35,8 @@ $(document).ready(function() {
 
     // Number randomizer for the random number and each crystal
 
-    var randomNumber = Math.floor((Math.random() * 120) + 19);
-    console.log(randomNumber);
-    var crystal1Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal1Value);
-    var crystal2Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal2Value);
-    var crystal3Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal3Value);
-    var crystal4Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal4Value);
+
+
 
     $("#match").html(randomNumber);
 
