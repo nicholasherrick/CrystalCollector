efaaac2c01f460c0f7984339594e6a89c8a2ab0d
Nick Herrick <nth.herrick@gmail.com>
2019-10-26 11:08:08 -0400

    added important info to console.log cleaned up comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,49 +2,53 @@
 
 $(document).ready(function() {
 
-    // Declare Variables
+    // Declare mathmatical variables
 
     var randomNumber = Math.floor((Math.random() * 120) + 19);
-    console.log(randomNumber);
+    console.log("Random Number = " + randomNumber);
     var crystal1Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal1Value);
+    console.log("Crystal 1 = " + crystal1Value);
     var crystal2Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal2Value);
+    console.log("Crystal 2 = " + crystal2Value);
     var crystal3Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal3Value);
+    console.log("Crystal 3 = " + crystal3Value);
     var crystal4Value = Math.floor((Math.random() * 12) + 1);
-    console.log(crystal4Value);
+    console.log("Crystal 4 = " + crystal4Value);
 
-    // Win/Loss and Total Score
+    // Declare  Win/Loss and total score variables
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
 
-    // function to reset game
+    // Function to reset game
 
     function resetGame() {
         randomNumber = Math.floor((Math.random() * 120) + 19);
-        console.log(randomNumber);
+        console.log("Random Number = " + randomNumber);
         wins = 0;
         losses = 0;
         crystal1Value = Math.floor((Math.random() * 12) + 1);
-        console.log(crystal1Value);
+        console.log("Crystal 1 = " + crystal1Value);
         crystal2Value = Math.floor((Math.random() * 12) + 1);
-        console.log(crystal2Value);
+        console.log("Crystal 2 = " + crystal2Value);
         crystal3Value = Math.floor((Math.random() * 12) + 1);
-        console.log(crystal3Value);
+        console.log("Crystal 3 = " + crystal3Value);
         crystal4Value = Math.floor((Math.random() * 12) + 1);
-        console.log(crystal4Value);
+        console.log("Crystal 4 = " + crystal4Value);
         totalScore = 0;
+        // Updates HTML with newly generated variables
         $("#match").html(randomNumber);
         $("#score-number").html(totalScore);
     }
+
+    // Updates the HTML page with global variables
 
     $("#match").html(randomNumber);
     $("#wins").html(wins);
     $("#losses").html(losses);
 
     // Functions for clicking on each crystal
+
     $("#crystal-1").on("click", function() {
         totalScore = totalScore + crystal1Value;
         $("#score-number").text(totalScore);
